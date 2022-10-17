25af7e7f7058c0de3696ff593567fdead7444ec2
최강준 <zopqdkak@naver.com>
2022-10-17 22:48:39 +0900

    test

---
+++ src/test/java/com/toyproject/wrightwyatt/WrightwyattApplicationTests.java
@@ -0,0 +1,13 @@
+package com.toyproject.wrightwyatt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WrightwyattApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
